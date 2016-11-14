3bcc62c42a00d2b19996dc473441f9fab5a41700
unknown <nehsoni@NEHSONI-DTAMR.partnet.cisco.com>
2016-11-15 00:37:27 +0530

    Java Code

---
+++ CRUDHibernatAnnotation/src/com/crud/hibernate/xml/StudentControllerServlet.java
@@ -0,0 +1,170 @@
+package com.crud.hibernate.xml;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/StudentControllerServlet")
+public class StudentControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+			// read the command parameter
+			String theCommand = request.getParameter("command");
+			
+			// if the command is missing,then default to listing the student
+			if (theCommand == null) {
+				theCommand = "LIST";
+			}
+
+			// route to the appropriate method
+			switch (theCommand) {
+			
+			case "LIST":
+				listStudents(request, response);
+				break;			
+
+			case "LOAD":
+				loadSudents(request, response);
+				break;			
+
+			case "DELETE":
+				deleteStudent(request, response);
+				break;
+
+			}
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		try {
+			// read the command parameter
+			String theCommand = request.getParameter("command");
+			
+			// if the command is missing,then default to listing the student
+			if (theCommand == null) {
+				theCommand = "LIST";
+			}
+
+			// route to the appropriate method
+			switch (theCommand) {
+			case "LIST":
+				listStudents(request, response);
+				break;
+
+			case "ADD":
+				addStudents(request, response);
+				break;
+				
+			case "UPDATE":
+				updateStudent(request, response);
+				break;				
+			}
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	private void deleteStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		// read the student id from the dat
+		String theId = request.getParameter("studentId");
+
+		// delete student from database
+		StudentDbUtil.deleteStudent(theId);
+
+		// send them back to the list student page
+		listStudents(request, response);
+	}
+
+	private void updateStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		// read the student form data
+		//int id = Integer.parseInt(request.getParameter("studentId"));
+		String id = request.getParameter("studentId");
+		System.out.println(id);
+		
+		String firstName = request.getParameter("first_name");
+		String lastName = request.getParameter("last_name");
+		String email = request.getParameter("email");
+
+		Student student3 = StudentDbUtil.getStudent(id);
+		
+		student3.setFirstName(firstName);
+		student3.setLastName(lastName);
+		student3.setEmail(email);
+		
+		// pass updated object to the database
+		StudentDbUtil.addOrUpdateStudent(student3);
+
+		// send them back to the list student page
+		listStudents(request, response);
+	}
+
+	private void loadSudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		// read the student id from the data
+		String theId = request.getParameter("studentId");
+		
+
+		// get the student from the database
+		Student student2 = StudentDbUtil.getStudent(theId);
+
+		// place student in the request attribute
+		request.setAttribute("THE_STUDENT", student2);
+
+		// send to jsp page: update-student-form.jsp
+		RequestDispatcher dispacher = request.getRequestDispatcher("/update-student.jsp");
+		dispacher.forward(request, response);
+
+	}
+
+	private void addStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		// get the form data from the form
+		String firstName = request.getParameter("first_name");
+		String lastName = request.getParameter("last_name");
+		String email = request.getParameter("email");
+
+		// create a new student using data
+		Student student1 = new Student(firstName, lastName, email);
+
+		// add new student to the database
+		StudentDbUtil.addOrUpdateStudent(student1);
+
+		// send back to the main page(the student list)
+		listStudents(request, response);
+
+	}
+
+	private void listStudents(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		// get the students from the studentDbUtil
+		List<Student> students = StudentDbUtil.listStudent();
+
+		// add the students to the request object
+		request.setAttribute("STUDENT_LIST", students);
+
+		// send to JSP
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/student-list.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
---
+++ CRUDHibernateXML/src/com/crud/hibernate/xml/StudentDbUtil.java
@@ -0,0 +1,91 @@
+package com.crud.hibernate.xml;
+
+import java.util.List;
+
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class StudentDbUtil {
+
+	// create a session factory
+	static SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
+	// create session
+	static Session session = null;
+
+	public static List<Student> listStudent() {
+
+		session = factory.getCurrentSession();
+
+		// start the transection
+		session.beginTransaction();
+
+		// query student
+		List<Student> theStudents = session.createQuery("from Student").list();
+
+		// commit the transaction
+		session.getTransaction().commit();
+
+		return theStudents;
+	}
+
+	public static void addOrUpdateStudent(Student student1) {
+
+		// create a session
+		session = factory.getCurrentSession();
+
+		// start the transaction
+		session.getTransaction().begin();;
+
+		// save the student
+		session.saveOrUpdate(student1);
+
+		// commit the transaction
+		session.getTransaction().commit();
+		
+	    }
+
+	public static Student getStudent(String theId) {
+
+		// get student id from the controller
+		int id = Integer.parseInt(theId);
+		System.out.println(id);
+
+		// create a session
+		session = factory.getCurrentSession();
+
+		// start the transaction
+		session.beginTransaction();
+
+		// load the student
+		Student myStudent = (Student) session.get(Student.class, id);
+
+		return myStudent;
+	}
+
+	public static void deleteStudent(String theId) {
+
+		// get student id from the controller
+		int id = Integer.parseInt(theId);
+
+		// get current session
+		session = factory.getCurrentSession();
+
+		// start the transaction
+		session.beginTransaction();
+
+		// create a session
+		Student student = (Student) session.get(Student.class, id);
+
+		session.delete(student);
+
+		// commit the transaction
+		session.getTransaction().commit();
+
+	}
+
+}
